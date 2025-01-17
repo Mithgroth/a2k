cb10da637e2507aef4ba997dbc77e66248c46d47
Emek Taydaş <mithgroth@gmail.com>
2025-01-17 14:32:30 +0300

    Add CommandLineHelpers

---
+++ src/Cli/Helpers/CommandLineHelpers.cs
@@ -0,0 +1,49 @@
+﻿using Spectre.Console;
+using System.CommandLine;
+using System.CommandLine.NamingConventionBinder;
+
+namespace a2k.Cli.Helpers;
+
+internal static class CommandLineHelpers
+{
+    internal static void Greet()
+    {
+        AnsiConsole.WriteLine();
+        AnsiConsole.Write(new FigletText("a2k").Color(Color.HotPink));
+        AnsiConsole.MarkupLine("[bold lime]Deploy .NET Aspire to Kubernetes![/]");
+        AnsiConsole.WriteLine();
+    }
+
+    private static IList<Option> ConfigureOptions()
+    {
+        var appHostOption = new Option<string>(
+            "--appHost",
+            description: "The path to the AppHost project folder",
+            getDefaultValue: Directory.GetCurrentDirectory);
+
+        var namespaceOption = new Option<string>(
+            "--namespace",
+            description: "The Kubernetes namespace to deploy to",
+            getDefaultValue: () => string.Empty);
+
+        return [appHostOption, namespaceOption];
+    }
+
+    internal static RootCommand WireUp<T1, T2>(Func<T1, T2, Task> handler)
+    {
+        var options = ConfigureOptions();
+
+        // Define the root command
+        var rootCommand = new RootCommand
+        {
+            // TODO: Find a better way to wire this up
+            options[0],
+            options[1]
+        };
+
+        rootCommand.Description = "a2k CLI: Deploy Aspire projects to Kubernetes";
+        rootCommand.Handler = CommandHandler.Create(handler);
+
+        return rootCommand;
+    }
+}
